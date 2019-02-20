2ed4407079602816a1d40c5e12277b522d03efd5
fredyballest <33910699+fredyballest@users.noreply.github.com>
2019-02-20 18:39:22 -0500

    Update views.py

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -5,7 +5,7 @@ class HomePageView(TemplateView):
     template_name = "core/home.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title':"Mi UNo Super Web Playground Edit1"})
+        return render(request, self.template_name, {'title':"Mi UNo Super Web Playground Edit2"})
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
